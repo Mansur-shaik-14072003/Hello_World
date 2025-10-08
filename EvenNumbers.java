10cf597f0b63d507e1f368108580d48534c23f7f
Mansur-Shaik <mansurshaik1316@gmail.com>
2025-10-08 15:13:57 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,23 @@
+// // ! write a java program to print even numbers from 1 to 100 by using while loop
+
+// public class EvenNumbers {
+//     public static void main(String[] args) {
+//         int i = 1;
+//         while (i <= 100) {
+//             if (i % 2 == 0) {
+//                 System.out.println(i);
+//             }
+//             i++;
+//         }
+//     }
+// }
+
+public class EvenNumbers{
+    public static void main(String args[]){
+        int i = 1;
+        while(i <=10){
+            System.out.println(".(welcome to  aditya university)");
+            i++;
+        }
+    }
+}
