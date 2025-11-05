827642d12e9aae5993d2aa4233c0bd50ef791d0d
Mansur-Shaik <mansurshaik1316@gmail.com>
2025-11-05 13:28:25 +0530

    Add files via upload

---
+++ CMDA.java
@@ -0,0 +1,27 @@
+// public class CMDA {
+//     public static void main(String[] args) {
+//         int s=0;
+//         for(int i = 0; i<args.length; i++){
+//             System.out.println(args[i]);
+//             s = s +Integer.parseInt(args[i]);
+//         }
+//         System.out.println("sum =" + s);
+//         System.out.println("Number of Input = " + args.length);
+    
+//     }
+// }
+public class CMDA{
+    public static void main(String[] args) {
+        int s=0;
+        for(int i=0;i<args.length;i++)
+         {
+            System.out.println(args[i]);
+
+            s=s+Integer.parseInt(args[i]);
+
+        }
+        System.out.println("sum="+s);
+        System.out.println("number of inputs"+args.length);
+        
+    }
+}
